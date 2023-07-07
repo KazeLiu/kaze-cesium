1ac16c94fe04568f5ac9d1077bc3a951a23366b7
kazeliu <kaze.liu@qq.com>
2023-07-07 17:59:19 +0800

    不知道怎么修复了混合class后方法找不到的问题

--- src/App.vue
+++ src/App.vue
@@ -9,25 +9,23 @@ import {onMounted, reactive} from "vue";
 import CesiumKaze from "./cesium/index.js";
 
 onMounted(async () => {
-  cesium.cesium = new CesiumKaze('cesium-dom');
-  await cesium.cesium.init();
-  cesium.cesium.addMarker({
+  let cesium = new CesiumKaze('cesium-dom')
+  cesium = new CesiumKaze('cesium-dom');
+  await cesium.init();
+  cesium.addMarker({
     iconImage: `/public/logo.jpg`,
     name: '原神！启动！',
     scale: 0.50,
     position: [112, 29]
   })
-  cesium.cesium.changeCollectionShowAndHide(false, 'haha')
-  cesium.cesium.on('contextMenu',res=>{
+  cesium.changeCollectionShowAndHide(false, 'haha')
+  cesium.on('contextMenu', res => {
     console.log(res)
   })
-  cesium.cesium.on('draw',res=>{
+  cesium.on('draw', res => {
     console.log(res)
     res[0].label.text.setValue("原神！启动！！")
   })
-})
-const cesium = reactive({
-  cesium: null,
 })
 </script>
 
